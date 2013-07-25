08b0e6fdf3b675230b1183e20baa030112c93c42
yang6n <yang6n@gmail.com>
2013-07-25 04:47:27 +0000

    HW4

--- web.js
+++ web.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var fs = require('fs');
 
-var app = express.createServer(express.logger());
+//var app = express.createServer(express.logger());
+var app = express(express.logger());
+
 
 app.get('/', function(request, response) {
   var data = fs.readFileSync('./index.html');
   response.send(data.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
